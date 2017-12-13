ed6d04468cb6067f4451ce0d5ca1a49362491eda
sujith savithry balan <sujithma1992@gmail.com>
2017-12-13 20:01:25 +0530

    world map component added

---
+++ src/app/dashboard-widget/world-map/world-map.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+declare const AmCharts;
+@Component({
+  	selector: 'cdk-world-map',
+  	templateUrl: './world-map.component.html',
+  	styleUrls: ['./world-map.component.scss']
+})
+export class WorldMapComponent implements OnInit {
+	
+	constructor() { }
+
+  	ngOnInit() {
+  		var map = AmCharts.makeChart( "chartdiv", {
+
+		  "type": "map",
+		  "theme": "light",
+		  "projection": "miller",
+
+		  "dataProvider": {
+		    "map": "worldLow",
+		    "getAreasFromMap": true
+		  },
+		  "areasSettings": {
+		    "autoZoom": true,
+		    "selectedColor": "#CC0000"
+		  },
+		} );
+  	}
+}
