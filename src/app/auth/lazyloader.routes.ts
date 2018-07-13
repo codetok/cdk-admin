ab24802223b5d3fb7e00128cce8b262c9393643a
hari <hkrishna@taxbackinternational.com>
2018-07-13 20:17:13 +0530

    removed links that are not working

--- src/app/auth/lazyloader.routes.ts
+++ src/app/auth/lazyloader.routes.ts
@@ -5,17 +5,16 @@ import { DashboardCrmComponent } from '../dashboard-crm/dashboard-crm.component'
 export const appRoutes: Routes = [{
     path: '', component: AuthComponent, children: [
         { path: 'dashboard', component: DashboardCrmComponent },
-        // { path: 'dashboard-account', loadChildren: '../dashboard-accounts/dashboard-accounts.module#DashboardAccountsModule' },
         { path: 'material-widgets', loadChildren: '../material-widgets/material-widgets.module#MaterialWidgetsModule' },
-        // { path: 'tables', loadChildren: '../tables/tables.module#TablesModule' },
-        // { path: 'maps', loadChildren: '../maps/maps.module#MapsModule' },
-        // { path: 'charts', loadChildren: '../charts/charts.module#ChartsModule' },
-        // { path: 'chats', loadChildren: '../chats/chat.module#ChatsModule' },
-        // { path: 'mail', loadChildren: '../mail/mail.module#MailModule' },
-        // { path: 'pages', loadChildren: '../pages/pages.module#PagesModule' },
-        // { path: 'forms', loadChildren: '../forms/forms.module#FormModule' },
-        // { path: 'guarded-routes', loadChildren: '../guarded-routes/guarded-routes.module#GuardedRoutesModule' },
-        // { path: 'editor', loadChildren: '../editor/editor.module#EditorModule' },
-        // { path: 'scrumboard', loadChildren: '../scrumboard/scrumboard.module#ScrumboardModule' },
+        { path: 'tables', loadChildren: '../tables/tables.module#TablesModule' },
+        { path: 'maps', loadChildren: '../maps/maps.module#MapsModule' },
+        { path: 'charts', loadChildren: '../charts/charts.module#ChartsModule' },
+        // { path: 'chats', loadChildren: '../chats/chat.module#ChatsModule' }, // fix this
+        //{ path: 'mail', loadChildren: '../mail/mail.module#MailModule' }, // fix this
+        { path: 'pages', loadChildren: '../pages/pages.module#PagesModule' },
+        { path: 'forms', loadChildren: '../forms/forms.module#FormModule' }, //fix this
+        { path: 'guarded-routes', loadChildren: '../guarded-routes/guarded-routes.module#GuardedRoutesModule' },
+        // { path: 'editor', loadChildren: '../editor/editor.module#EditorModule' }, 
+        { path: 'scrumboard', loadChildren: '../scrumboard/scrumboard.module#ScrumboardModule' },
     ]
 }];
