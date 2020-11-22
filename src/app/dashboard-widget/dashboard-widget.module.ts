670270e233a41b4875ca1a5384d316458417d53b
Fotso Teketchuet Stephane <lesauf@gmail.com>
2020-11-22 17:36:38 +0100

    Angular 10 upgrade completed. Replacing charts with ngx-charts

--- src/app/dashboard-widget/dashboard-widget.module.ts
+++ src/app/dashboard-widget/dashboard-widget.module.ts
@@ -4,25 +4,27 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DashcardComponent } from './dashcard/dashcard.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatChipsModule} from '@angular/material/chips';
 import { MatProgressBarModule} from '@angular/material/progress-bar';
-import { LineGraphComponent } from './line-graph/line-graph.component';
-import { BarGraphComponent } from './bar-graph/bar-graph.component';
-import { DoughnutGraphComponent } from './doughnut-graph/doughnut-graph.component';
 import { ProfileCardComponent } from './profile-card/profile-card.component';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { PricingPlanComponent } from './pricing-plan/pricing-plan.component';
 import { MatListModule } from '@angular/material/list';
 import { WeatherComponent } from './weather/weather.component';
 import { SharePriceComponent } from './share-price/share-price.component';
 import { RoundProgressbarComponent } from './round-progressbar/round-progressbar.component'; 
+import { LineGraphComponent } from './line-graph/line-graph.component';
+import { BarGraphComponent } from './bar-graph/bar-graph.component';
+import { DoughnutGraphComponent } from './doughnut-graph/doughnut-graph.component';
+import { PricingPlanComponent } from './pricing-plan/pricing-plan.component';
 import { RoundProgressModule } from 'angular-svg-round-progressbar';
 import { SalesListComponent } from './sales-list/sales-list.component';
 import { D3UsaComponent } from './d3-usa/d3-usa.component';
-import { Ng9OdometerModule } from 'ng9-odometer';
+// import { Ng9OdometerModule } from 'ng9-odometer';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
 import { WorldMapComponent } from './world-map/world-map.component';
 
 
@@ -31,6 +33,7 @@ import { WorldMapComponent } from './world-map/world-map.component';
     CommonModule,
     FlexLayoutModule,
     MatButtonModule,
+    MatCardModule,
     MatIconModule,
     MatTabsModule,
     MatToolbarModule,
@@ -38,7 +41,8 @@ import { WorldMapComponent } from './world-map/world-map.component';
     MatMenuModule,
     MatChipsModule,
     MatProgressBarModule,
-    Ng9OdometerModule.forRoot(),
+    // Ng9OdometerModule.forRoot(),
+    NgxChartsModule,
     RoundProgressModule,
     FormsModule,
     ReactiveFormsModule
