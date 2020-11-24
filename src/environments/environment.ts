692e11311a9ae82c563c58b4d845f8bc1fccf14e
Fotso Teketchuet Stephane <lesauf@gmail.com>
2020-11-24 11:20:42 +0100

    Init Firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,12 +6,14 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyBwlk0KMM75nBsZ8L7j9cZ5N5x0fwqfBjg',
-    authDomain: 'newproject-5d731.firebaseapp.com',
-    databaseURL: 'https://newproject-5d731.firebaseio.com',
-    projectId: 'newproject-5d731',
-    storageBucket: 'newproject-5d731.appspot.com',
-    messagingSenderId: '737689833575'
+    apiKey: "AIzaSyDYv02aLpqUgtJIP9CbT31mOHpu0a77xLM",
+    authDomain: "cdk-admin.firebaseapp.com",
+    databaseURL: "https://cdk-admin.firebaseio.com",
+    projectId: "cdk-admin",
+    storageBucket: "cdk-admin.appspot.com",
+    messagingSenderId: "422055031001",
+    appId: "1:422055031001:web:261c3f03c6dbe830df8ec8",
+    measurementId: "G-NYGXB63DCC"
   },
   mailApi: '/assets/list.json'
 };
